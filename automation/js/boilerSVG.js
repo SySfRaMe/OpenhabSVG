da6a6fc670402a75a0d0fcc93df0742e1495d9af
SySfRaMe <32898648+SySfRaMe@users.noreply.github.com>
2024-11-05 00:21:43 +0100

    Update boilerSVG.js
    Added IP as variable

--- automation/js/boilerSVG.js
+++ automation/js/boilerSVG.js
@@ -9,7 +9,7 @@ Code Start
 const temp_name = "BoilerTemperature";
 const url_name = "BoilerURL";
 const level_name = "BoilerLEVEL";
-
+const ip = "xxx.xxx.xxx.xxx";
 const min_temp = 40;
 const max_temp = 80;
 
@@ -52,7 +52,7 @@ if (procent == 0){
 offset1 = Math.round(offset1);
 offset2 = Math.round(offset2);
 procent = Math.round(procent);
-let url = "http://10.0.0.10:8080/static/boiler.html?offset1=" + offset1 + "%&offset2=" + offset2 + "%&color1=" + color1 + "&color2=" + color2;
+let url = "http://" + ip + ":8080/static/boiler.html?offset1=" + offset1 + "%&offset2=" + offset2 + "%&color1=" + color1 + "&color2=" + color2;
 items.getItem(url_name).sendCommand(url);
 items.getItem(level_name).sendCommand(procent);
 
